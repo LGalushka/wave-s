746e05b2df993ed17579095d48a2447a1cea1614
LGalushka <liliya2615@gmail.com>
2026-03-09 11:14:38 +0300

    feat: create NavBar component and navLinks constants

---
+++ src/components/layout/Header/NavBar.tsx
@@ -0,0 +1,55 @@
+import { Button } from "@/components/ui/Button";
+import { Menu, Phone } from "lucide-react";
+import { NavLink } from "react-router-dom";
+import { navLinks } from "./navLinks";
+
+interface NavBarProps {
+  onCallClick: () => void;
+}
+
+export const NavBar = ({ onCallClick }: NavBarProps) => {
+  return (
+    <div className="w-full 'px-4' 'px-3'">
+      <div className="max-w-7xl mx-auto flex items-center justify-between">
+        {/** Бургер для мобильного */}
+        <button className="lg: hidden text-text-white">
+          <Menu size={24} />
+        </button>
+
+        {/**Лого */}
+        <NavLink to="/" className="shrink-0">
+          <img src="../../../assets/logo.png" alt="Wave-S" width={190} height={62} />
+        </NavLink>
+
+        {/**Навигация для десктоп */}
+        <nav className="hidden lg:flex items-center gap-8">
+          {navLinks.map((link) => (
+            <NavLink
+              key={link.path}
+              to={link.path}
+              className={({ isActive }) => `
+      text-text-white text-base transition-colors hover:text-text-accent
+      ${isActive ? "border-b-2 border-text-accent pb-0.5 rounden-sm" : ""}
+      `}
+            >
+              {link.label}
+            </NavLink>
+          ))}
+        </nav>
+
+        {/**Кнопка декстоп / иконка мобильн */}
+        <div>
+          <div className="hidden lg:block">
+            <Button variant="header" onClick={onCallClick}>
+              Заказать звонок
+            </Button>
+          </div>
+
+          <a href="tel:+78007002060" className="lg:hidden text-text-white">
+            <Phone size={24} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/layout/Header/navLinks.tsx
@@ -0,0 +1,7 @@
+export const navLinks = [
+  { label: "Оборудование", path: "/catalog" },
+  { label: "Объекты", path: "/objects" },
+  { label: "О компании", path: "/about" },
+  { label: "Пресса о нас", path: "/press" },
+  { label: "Контакты", path: "/contacts" },
+];
